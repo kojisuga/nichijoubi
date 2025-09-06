b5d6de0615655f83b42be635dcd4267c0f5ea141
kojisuga <129404821+kojisuga@users.noreply.github.com>
2025-09-06 17:06:45 +0900

    共有

--- nichijoubiTheme/index.php
+++ nichijoubiTheme/index.php
@@ -2,7 +2,6 @@
 <head>
 <meta></meta>
 
-<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css?<?php echo file_date(get_template_directory() . '/style.css'); ?>" type="text/css" />
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/main.css?<?php echo file_date(get_template_directory() . '/css/main.css'); ?>" type="text/css" />
 
 <?php require "common.php"; ?> 																	<!-- Call common.php ---->
